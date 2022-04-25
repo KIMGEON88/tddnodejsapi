02245b68f75eaf831beb806ec5ebe16bf9fe10bc
KIMGEON88 <geonking88@gmail.com>
2022-04-25 14:58:04 +0900

    bodyparser

--- index.js
+++ index.js
@@ -34,29 +34,58 @@
 
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
 
-const users = [
+let users = [
   { id: 1, name: "kim" },
   { id: 2, name: "lee" },
   { id: 3, name: "park" },
 ];
 
 app.use(morgan("dev"));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/users", function (req, res) {
-  res.send("users list");
+  req.query.limit = req.query.limit || 10;
+  const limit = parseInt(req.query.limit, 10);
+  if (Number.isNaN(limit)) return res.status(400).end();
+
+  res.json(users.slice(0, limit));
 });
 
-app.post("/users", function (req, res) {
-  res.send(users);
+app.get("/users/:id", function (req, res) {
+  const id = parseInt(req.params.id, 10);
+
+  if (Number.isNaN(id)) return res.status(400).end();
+
+  const user = users.filter((user) => user.id === id)[0];
+  if (!user) return res.status(404).end();
+  res.json(user);
+});
+
+app.delete("/users/:id", function (req, res) {
+  const id = parseInt(req.params.id, 10);
+
+  if (Number.isNaN(id)) return res.status(400).end();
+
+  users = users.filter((user) => user.id !== id);
+  res.status(204).end();
+});
+
+app.post("/users", (req, res) => {
+  req.body.name;
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+module.exports = app;
